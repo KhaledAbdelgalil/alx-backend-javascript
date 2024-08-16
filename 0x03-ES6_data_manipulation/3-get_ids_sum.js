fbb9b239c2c391ffa5e6d1adfc8f1c1550b886e4
Khaled_Mansour <36304731+KhaledAbdelgalil@users.noreply.github.com>
2024-08-16 23:09:16 +0100

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,8 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
-      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
-      0,
+      (sum, curStudent) => sum + curStudent.id,
+      0
     );
   }
   return 0;
